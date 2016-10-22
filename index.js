55c65bfba8a5a678cd6b8648102f939548e2ae3a
justin kadima <justinkadima@gmail.com>
2016-10-22 15:45:34 +0300

    setup

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
+var Resource=require('deployd/lib/resource');
+var util=require('util');
 
+
+
+
+function ElasticEmail(){
+Resource.apply(this, arguments);
+
+
+
+}
+
+util.inherits(ElasticEmail,Resource);
+
+
+ElasticEmail.prototype.clientGeneration=true;
+
+ElasticEmail.basicDashboard={
+ settings:[
+  {
+      name:'username',
+      type:'text',
+      description:'Elastic account username'
+  },
+  {
+      name:'password',
+      type:'text',
+      description:'Elastic account password'
+  },
+  {
+      name:'defaultFROM',
+      type:'text',
+      description:'The default FROM email address'
+   },
+   {
+        name:'defaultSubject',
+        type:'text',
+        description:'The default subject '
+    },
+    {
+         name:'developmentMode',
+        type:'checkbox',
+        description:'if true then it will print emails to console instead of sending them'
+
+    }
+ ]
+
+
+};
+
+
+ElasticEmail.prototype.handle=function(ctx,next){
+
+    /*if(ctx.req && ctx.req.method!='POST'){
+        return next();
+    }*/
+    var options=ctx.body || {};
+
+    console.log(options);
+
+
+}
+
+module.exports=ElasticEmail;
