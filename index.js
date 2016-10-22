da20c6ccd2e8d31214a579d52245e93aeead6d50
justin kadima <justinkadima@gmail.com>
2016-10-22 17:45:20 +0300

    added Elastic Email integration

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var Resource=require('deployd/lib/resource');
 var util=require('util');
+var qs=require('querystring');
+var https=require('https');
 
+var Resource=require('deployd/lib/resource');
 
 
 
 function ElasticEmail(){
-Resource.apply(this, arguments);
-
-
+    Resource.apply(this, arguments);
 
 }
 
@@ -24,12 +24,17 @@ ElasticEmail.basicDashboard={
       description:'Elastic account username'
   },
   {
-      name:'password',
+      name:'key',
       type:'text',
-      description:'Elastic account password'
+      description:'Elastic api key'
   },
   {
-      name:'defaultFROM',
+      name:'defaultFromName',
+      type:'text',
+      description:'The default FROM name'
+   },
+   {
+      name:'defaultFromEmail',
       type:'text',
       description:'The default FROM email address'
    },
@@ -52,14 +57,98 @@ ElasticEmail.basicDashboard={
 
 ElasticEmail.prototype.handle=function(ctx,next){
 
-    /*if(ctx.req && ctx.req.method!='POST'){
+   /*if(ctx.req && ctx.req.method!='POST'){
         return next();
     }*/
+
+    if(!this.config.username || !this.config.key){
+        console.log('Elastic username & key missing!');
+        next();
+    }
+
+
     var options=ctx.body || {};
 
     console.log(options);
 
+    options.fromName=options.fromName || this.config.defaultFromName;
+    options.fromEmail=options.fromEmail || this.config.defaultFromEmail;
+    options.subject=options.subject || this.config.defaultSubject;
 
+    console.log(options);
+
+    var errors={};
+
+    if(!options.to){
+        errors.to=' \'to\' is required';
+
+    }
+    if(!options.fromEmail){
+        errors.from=' \'from\' is required';
+    }
+    if(!options.subject){
+        errors.subject=' \'subject\' is required';
+    }
+
+    if(!options.text && !options.html){
+        errors.content=' \'text\' or  \'html\' is required ';
+    }
+
+    if(errors.length>0){
+        return ctx.done({statusCode: 400, errors: errors});
+    }
+
+
+    var post_data = querystring.stringify({
+		'username' : this.config.username,
+		'api_key': this.config.key,
+		'from': options.fromEmail,
+		'from_name' : options.fromName,
+		'to' : options.to,
+		'subject' : options.subject,
+		'body_html' : options.text,
+		'body_text' : options.html
+	});
+
+
+    if(this.config.developmentMode){
+        console.log(post_data);
+        ctx.done(null);
+    }
+
+    
+	var post_options = {
+		host: 'api.elasticemail.com',
+		path: '/mailer/send',
+		port: '443',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': post_data.length
+		}
+	};
+
+	var result = '';
+	// Create the request object.
+	var post_req = https.request(post_options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			result = chunk;
+		});
+		res.on('error', function (e) {
+			result = 'Error: ' + e.message;
+		});
+	});
+
+	// Post to Elastic Email
+	post_req.write(post_data);
+	post_req.end();
+	
+    ctx.done(null,{message: result});
 }
+
+
+
+
 
 module.exports=ElasticEmail;
